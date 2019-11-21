a435fbd516fb20a6f73fef6962db6ba34064f23d
Travis CI <travis@travis-ci.org>
2019-11-21 14:44:26 -0500

    spring cleaning in robot

--- src/main/java/frc/team5115/Robot/Robot.java
+++ src/main/java/frc/team5115/Robot/Robot.java
@@ -2,26 +2,17 @@ package frc.team5115.Robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.team5115.Subsystems.Intake;
 import frc.team5115.Subsystems.Cart;
 import frc.team5115.Subsystems.Shooter;
 
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
   public static Intake intake;
   public static Cart costco;
   public static Shooter shooter;
 
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
     intake = new Intake();
     costco = new Cart();
     shooter = new Shooter();
@@ -33,22 +24,10 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
   }
 
   @Override
   public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
   }
 
   @Override
